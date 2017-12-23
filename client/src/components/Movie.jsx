0c741c9bcfc5de9f9d10e791be8ee3be36fe2dbe
Jeroen <jeroent94@gmail.com>
2017-12-23 12:02:45 -0800

    Movie titles rendered to DOM

---
+++ client/src/components/Movie.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+
+export const Movie = (props) => (
+<li>{props.movie.title}</li>
+)
+
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,17 +1,35 @@
 import React from 'react';
 import ReactDOM  from 'react-dom';
+import {Movie} from './components/Movie.jsx';
 
+
+var movieData = [
+  {title: 'Mean Girls'},
+  {title: 'Hackers'},
+  {title: 'The Grey'},
+  {title: 'Sunshine'},
+  {title: 'Ex Machina'},
+];
 
 class MovieList extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: movieData
+    }
   }
 
   render() {
     return (
-      <div></div>
+      <ul>
+        {this.state.movies.map(currentMovie =>
+          <Movie movie={currentMovie} key={currentMovie.title} />)
+        }
+      </ul>
     )
   }
 }
 
-ReactDOM.render( <MovieList />, document.getElementById('app'));
+
+ReactDOM.render(<MovieList />, document.getElementById('app'));
+
